c8d399064420b71dcea5edda01de0fc3f98b089e
Satya Ranjan Mallick <88711085+satyamallick7@users.noreply.github.com>
2022-07-31 17:59:08 +0400

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
   return (
     <header className={`${bg ? 'bg-tertiary h-20' :  'h-24'} flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
         <div className="container mx-auto h-full flex items-center justify-between">
-            <a href="#">
+            <a href="home">
                 <h1 className='font-paci text-3xl'>Satya Mallick</h1>
             </a>
             <div className='hidden lg:block'>
